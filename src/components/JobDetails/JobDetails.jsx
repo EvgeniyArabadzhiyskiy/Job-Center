4be1d4c7278ef2704ee37cb8e04fa28cb182e320
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-11-11 22:08:59 +0200

    Update JobDetails.jsx

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -20,31 +20,31 @@ const JobDetails = ({ jobs }) => {
 
   return (
     <div className="max-w-[1368px] my-0 mx-auto ">
-      <div className="flex gap-[60px] max-w-[1216px] min-h-screen my-0 mx-auto ">
-        <div className="w-2/3">
-          <div className="flex justify-between items-center mb-[50px]">
+      <div className="bg-[#6db676] pt-[24px] max-w-[1216px] min-h-screen my-0 mx-auto md:bg-[#70b9ad]  md:flex md:gap-[60px] md:pt-[50px]  ">
+        <div className="w-[100%]  px-[15px]  md:w-2/3 md:p-0" >
+          <div className="mb-[32px]   md:flex md:justify-between md:items-center md:mb-[50px]">
             <h1 className="text-[28px] font-bold leading-[34px] text-[#3a4562] ">
               Job Details
             </h1>
-            <ul className="flex justify-between items-center gap-[24px] mr-[50px] text-[18px] font-normal leading-[24px] text-[#3a4562]  ">
+            <ul className="flex  items-center md:justify-between gap-[36px] mt-[35px]  md:gap-[24px] md:mt-0  md:mr-[50px] text-[18px] font-normal leading-[24px] text-[#3a4562]  ">
               <li className="links-item">Save Results</li>
               <li className="links-item">Share</li>
             </ul>
           </div>
 
           <button
-            className="text-[12px] font-semibold leading-[16px] text-[#3a4562] uppercase py-[18px] px-[30px]  rounded-[8px] border-none bg-[#384564] text-[#fff] cursor-pointer"
+            className="hidden md:inline-block   text-[12px] font-semibold leading-[16px] text-[#3a4562] uppercase py-[18px] px-[30px]  rounded-[8px] border-none bg-[#384564] text-[#fff] cursor-pointer"
             type="button"
           >
             Apply Now
           </button>
 
           <section className="flex flex-col mt-[32px] ">
-            <div className="grid grid-cols-[1fr_minmax(150px,_auto)] gap-x-[20px] ">
-              <h2 className="text-[24px] font-bold leading-[30px] text-[#3a4562]">
+            <div className="grid items-center grid-cols-[repeat(1,1fr_minmax(150px,_auto))] grid-rows-[repeat(2,auto)]  gap-x-[20px] gap-y-[35px] md:items-start md:gap-y-0 ">
+              <h2 className="col-span-2 md:col-span-1 text-[24px] font-bold leading-[30px] text-[#3a4562]">
                 {title}
               </h2>
-              <div className="sulary-wrapper">
+              <div className="order-1 md:order-none">
                 <p className="text-[20px] font-bold leading-[25px] text-[#3a4562]">
                   {getSalary(salary)}
                 </p>
@@ -52,11 +52,12 @@ const JobDetails = ({ jobs }) => {
                   Brutto, per year
                 </p>
               </div>
-            </div>
-
-            <p className="text-[18px] font-normal leading-[24px] text-[#d25014e8] mt-[8px]">
+              <p className="text-[18px] font-normal leading-[24px] text-[#d25014e8] md:mt-[8px]">
               Posted 2 days ago
             </p>
+            </div>
+
+            
 
             <div className="response">
               <h3 className="text-[20px] font-bold leading-[25px] text-[#3a4562] my-[15px] ">
@@ -99,7 +100,7 @@ const JobDetails = ({ jobs }) => {
                 Our physicians enjoy a wide range of benefits, including:
               </p>
 
-              <ul className="text-[18px] font-normal leading-[24px] text-[#3a4562] list-[square]">
+              <ul className=" ml-[20px] md:ml-0  text-[18px] font-normal leading-[24px] text-[#3a4562] list-[square]">
                 <li className="compensation-item">
                   Very competitive compensation package with bonuses
                 </li>
@@ -118,14 +119,14 @@ const JobDetails = ({ jobs }) => {
           </section>
 
           <button
-            className="text-[12px] font-semibold leading-[16px] text-[#3a4562] uppercase py-[18px] px-[30px]  rounded-[8px] border-none bg-[#384564] text-[#fff] cursor-pointer"
+            className="my-0 mx-[auto] block md:inline-block md:m-0   text-[12px] font-semibold leading-[16px] text-[#3a4562] uppercase py-[18px] px-[30px]  rounded-[8px] border-none bg-[#384564] text-[#fff] cursor-pointer"
             type="button"
           >
             Apply Now
           </button>
 
           <section className="mt-[86px] ">
-            <h2 className="text-[28px] font-semibold leading-[33px] text-[#3a4562] mb-[23px] ">
+            <h2 className="text-[28px] font-bold leading-[33px] text-[#3a4562] mb-[23px] ">
               Additional Info
             </h2>
 
@@ -162,15 +163,15 @@ const JobDetails = ({ jobs }) => {
             </div>
           </section>
 
-          <section className="mb-[107px]">
+          <section className="mb-[85px]">
             <h2 className="text-[28px] font-bold leading-[34px] text-[#3a4562] mt-[87px] mb-[30px] ">
               Atached Images
             </h2>
 
-            <div className="flex gap-x-[10px]">
+            <div className="grid gap-y-[20px] justify-items-center  items-center grid-cols-[repeat(2,1fr)] md:grid-cols-[repeat(3,1fr)] gap-x-[10px]">
               {pictures.map((img, idx) => (
                 <img
-                  className="w-[210px] h-[115px] rounded-[8px]"
+                  className=" rounded-[8px]"
                   src={img}
                   alt={name}
                   key={idx}
@@ -179,7 +180,8 @@ const JobDetails = ({ jobs }) => {
             </div>
           </section>
         </div>
-        <section className="h-[100%] bg-[#894141] ">
+        <section className="w-[100%]  md:w-[400px] h-[502px] md:h-[436px]">
+        <h3 class="md:hidden text-[28px] font-bold leading-[34px] text-[#3a4562] mb-[32px] pl-[15px] ">Contacts</h3>
           {<GoogleMap location={location} />}
         </section>
       </div>
