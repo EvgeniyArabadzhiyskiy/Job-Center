41a42699b46522eecfda2fc218faaa0a1947f349
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-11-12 22:11:56 +0200

    Create Stars.jsx

---
+++ src/components/Stars/Stars.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+import { AiFillStar } from 'react-icons/ai';
+import { AiOutlineStar } from 'react-icons/ai';
+
+const stars = [1, 2, 3, 4, 5];
+
+const Stars = () => {
+  const [activeIdx, setActiveIdx] = useState(-1);
+
+  const handleClick = idx => {
+    if (idx === activeIdx) {
+      setActiveIdx(-1);
+      return;
+    }
+    setActiveIdx(idx);
+  };
+  return (
+    <>
+      <ul className="flex">
+        {stars.map((item, idx) => {
+          return (
+            <li
+              className="flex items-center justify-center"
+              key={idx}
+              onClick={() => handleClick(idx)}
+            >
+              {idx <= activeIdx ? (
+                <AiFillStar size={18} fill={'#38415D'} />
+              ) : (
+                <AiOutlineStar size={18} />
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Stars;
