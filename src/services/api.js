bd7204b2238358a0bcb1c1d46d1569775bc62459
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-11-12 22:11:50 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,51 +5,5 @@ const ACCESS_TOKEN = 'access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu';
 
 export async function getJobList() {
   const { data } = await axios.get(`${BASE_URL}?${ACCESS_TOKEN}`);
-  console.log('getJobList ~ data', data);
   return data;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import axios from 'axios';
-
-// const BASE_URL = 'https://pixabay.com/api/';
-// const API_KEY = '26298929-dc8db63efad38f2c4177a32d6';
-// const OPTIONS = 'image_type=photo';
-
-// export async function getJobList() {
-//   const response = await axios(
-//     `${BASE_URL}?key=${API_KEY}&q=cat&${OPTIONS}`
-//   );
-
-//   return response.data;
-// }
