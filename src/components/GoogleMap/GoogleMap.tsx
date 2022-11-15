dae5cf51d72518ac5b76e9f3fea3f559670745c8
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-11-15 21:53:53 +0200

    Update GoogleMap.tsx

--- src/components/GoogleMap/GoogleMap.tsx
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -6,6 +6,9 @@ import Map from './Map';
 const { REACT_APP_GOOGLE_MAP_APIkey } = process.env;
 console.log("REACT_APP_GOOGLE_MAP_APIkey", REACT_APP_GOOGLE_MAP_APIkey);
 
+const fff = REACT_APP_GOOGLE_MAP_APIkey
+console.log("fff", fff);
+
 interface IGoogleMapProps {
   location: {
     lat: number;
@@ -17,7 +20,7 @@ interface IGoogleMapProps {
 
 const GoogleMap: React.FC<IGoogleMapProps> = ({ location, getAddress }) => {
   return (
-    <Wrapper apiKey={REACT_APP_GOOGLE_MAP_APIkey}>
+    <Wrapper apiKey={fff}>
       <Map lat={location.lat} lng={location.long} getAddress={getAddress} />
     </Wrapper>
   );
