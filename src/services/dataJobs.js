8603ae0c8ce3c6bd2250794209802c14dc6feea5
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-11-15 20:14:18 +0200

    Create dataJobs.js

---
+++ src/services/dataJobs.js
@@ -0,0 +1,235 @@
+const array = [
+  {
+    id: '1',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Full time', 'Part Time'],
+    name: 'Djon',
+    benefits: ['Pay vocations', 'Flexible hours'],
+    address: '69 Blende Jardine Place',
+    createdAt: '2022-11-04T01:38:26.141Z',
+    location: { lat: 47.8223765, long: 35.1682415},
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+
+  {
+    id: '2',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Full time', 'Part Time', 'Temporary'],
+    name: 'Djon',
+    benefits: ['Flexible Shedule', 'Flexible hours'],
+    address: '58 Blende Jardine Place',
+    createdAt: '2022-11-10T01:38:26.141Z',
+    location: { lat: 48.4628031, long: 35.0448411 },
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+
+  {
+    id: '3',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Part Time', 'Temporary', 'Full time'],
+    name: 'Djon',
+    benefits: ['Relocation Assistance', 'Flexible Shedule'],
+    address: '78 Blende Jardine Place',
+    createdAt: '2022-11-04T01:38:26.141Z',
+    location: { lat: 49.5908774, long: 34.5503844 },
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+
+  {
+    id: '4',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Temporary', 'Part Time', 'Full time'],
+    name: 'Djon',
+    benefits: ['Pay vocations', 'Flexible hours'],
+    address: '41 Blende Jardine Place',
+    createdAt: '2022-11-01T01:38:26.141Z',
+    location: { lat: 50.4345171, long: 30.5578312 },
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+
+  {
+    id: '5',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Full time', 'Temporary'],
+    name: 'Djon',
+    benefits: ['Pay vocations', 'Flexible Shedule'],
+    address: '26 Blende Jardine Place',
+    createdAt: '2022-11-09T01:38:26.141Z',
+    location: { lat: 45.804124, long: 28.139488 },
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+
+  {
+    id: '6',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Full time', 'Temporary', 'Part Time'],
+    name: 'Djon',
+    benefits: ['Pay vocations', 'Flexible hours'],
+    address: '85 Blende Jardine Place',
+    createdAt: '2022-11-11T01:38:26.141Z',
+    location: { lat: 46.804124, long: 27.139488 },
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+
+  {
+    id: '7',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Part Time', 'Full time'],
+    name: 'Djon',
+    benefits: ['Relocation Assistance', 'Flexible Shedule'],
+    address: '12 Blende Jardine Place',
+    createdAt: '2022-11-08T01:38:26.141Z',
+    location: { lat: 49.804124, long: 27.139488 },
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+
+  {
+    id: '8',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Full time'],
+    name: 'Djon',
+    benefits: ['Pay vocations', 'Flexible hours'],
+    address: '123 Blende Jardine Place',
+    createdAt: '2022-11-04T01:38:26.141Z',
+    location: { lat: 50.804124, long: 30.139488 },
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+
+  {
+    id: '9',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Full time', 'Part Time'],
+    name: 'Djon',
+    benefits: ['Pay vocations', 'Relocation Assistance'],
+    address: '45 Blende Jardine Place',
+    createdAt: '2022-11-14T01:38:26.141Z',
+    location: { lat: 48.804124, long: 27.139488 },
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+
+  {
+    id: '10',
+    title:
+      'Ut veniam occaecat aute adipisicing eiusmod non pariatur enim enim cupidatat nulla .',
+    description:
+      'Reprehenderit Lorem consectetur non et minim adipisicing deserunt. Ipsum reprehenderit do p',
+    pictures: [
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+      'https://picsum.photos/200/300',
+    ],
+    employment_type: ['Full time', 'Reprehenderit'],
+    name: 'Djon',
+    benefits: ['Relocation Assistance', 'Flexible hours'],
+    address: '765 Blende Jardine Place',
+    createdAt: '2022-11-12T01:38:26.141Z',
+    location: { lat: 46.804124, long: 26.139488 },
+    phone: '+97117307890',
+    email: 'sureplex@gmail.bo',
+    updatedAt: '2012-05-05T01:38:26.141Z',
+    salary: '60k-71k',
+  },
+];
+
+export default array;
+
+
